e78f2be9bfc17fb813a5434485434ffffef2c966
Aleksa <alexaw32@hotmail.com>
2025-05-23 16:02:12 +0200

    230525-finalvite22

--- portfolio2025/js/script.js
+++
@@ -1,7 +0,0 @@
-/*  typing animation */
-var typed = new Typed(".typing", {
-    strings:["","Web Designer","Web Developer","Graphic Designer", "Youtuber"],
-    typeSpeed: 100,
-    BackSpeed: 50,
-    loop: true
-});
